2b3989b125254a3c5ccaf2656ff0be797995285f
风清扬 <978924688@qq.com>
2020-03-29 21:07:23 +0800

    Revert "Merge commit 'ce4a96416980647e08d96415bf6adcea262b9f07'"
    This reverts commit 71e37843ff8941770482746e5c4a0a47899fad3c, reversing
    changes made to 57361520472ac460ed70921c272bcb83358decd6.
    
    # Conflicts:
    #	Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -28,4 +28,34 @@ int main(void)
     system("pause");
     return 0;
 
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main(void)
+{
+    AbstractCalculator *p = new Add(3,7);
+    cout << p->m_Val_1 << "+" << p->m_Vla_2 << "=" << p->get_Result() << endl;
+
+    p = new Minus(4,8);
+    cout << p->m_Val_1 << "-" << p->m_Vla_2 << "=" << p->get_Result() << endl;
+
+    p = new Mul(10,2);
+    cout << p->m_Val_1 << "x" << p->m_Vla_2 << "=" << p->get_Result() << endl;
+    delete p;
+
+    cout << "Git test !<<endl";
+    cout << "11:02" << endl;
+    cout << "branch 20200328" << endl;
+    
+    system("pause");
+    return 0;
+
+    2020-03-29
+
 }
