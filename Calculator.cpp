3dc80e065de8e86f3557ec0238d5ccb8f4e5429f
风清扬 <44631015+978924688@users.noreply.github.com>
2020-03-29 16:37:18 +0800

    version-04

--- Calculator.cpp
+++ Calculator.cpp
@@ -24,9 +24,5 @@ int main(void)
     cout << "Git test !<<endl";
     cout << "11:02" << endl;
     cout << "branch 20200328" << endl;
-    cout << "branch 11:29" << endl;
-    cout << "branch 11:39" << endl;
 
-    system("pause");
-    return 0;
 }
